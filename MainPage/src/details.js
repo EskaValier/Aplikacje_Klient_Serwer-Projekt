688dabe47290d3943688b72d10c2ba4aa8ff766d
EskaValier <d.soczek1999@gmail.com>
2022-01-22 21:20:21 +0100

    Details

--- MainPage/src/details.js
+++ MainPage/src/details.js
@@ -1,16 +1,16 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
-console.log(id);
+// console.log(id);
 
 const url = "https://api.magicthegathering.io/v1/cards/" + id;
-console.log(url);
+// console.log(url);
 fetch(url)
     .then(response => response.json())
     .then(response => {
         // console.log(response)
         let card = response.card;
-        console.log(card)
+        // console.log(card)
 
         document.getElementById('name').innerText = card.name;
         var image = new Image();
@@ -40,5 +40,4 @@ fetch(url)
         // cardThughness
         // cardManaCost
         // cardText
-        
     })
