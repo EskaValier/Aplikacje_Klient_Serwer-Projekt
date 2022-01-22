9ab379cb5ed33323d514db350ce3798b363bacba
EskaValier <d.soczek1999@gmail.com>
2022-01-22 20:23:47 +0100

    details page

--- MainPage/src/details.js
+++ MainPage/src/details.js
@@ -12,5 +12,33 @@ fetch(url)
         let card = response.card;
         console.log(card)
 
+        document.getElementById('name').innerText = card.name;
+        var image = new Image();
+        image.src = card.imageUrl;
+        image.alt = "No Graphic";
+        document.getElementById('cardImage').appendChild(image);
+        document.getElementById('cardType').innerText = card.type == undefined ? "NotSet" : card.type;
+        document.getElementById('cardSubtype').innerText = card.subtypes == undefined ? "NotSet" : card.subtypes;
+        document.getElementById('cardColor').innerText = card.colors == undefined ? "NotSet" : card.colors;
+        document.getElementById('cardSupertypes').innerText = card.supertypes == undefined ? "NotSet" : card.supertypes;
+        document.getElementById('cardRarity').innerText = card.rarity == undefined ? "NotSet" : card.rarity;
+        document.getElementById('cardSet').innerText = card.set == undefined ? "NotSet" : card.set;
+        document.getElementById('cardPower').innerText = card.power == undefined ? "NotSet" : card.power;
+        document.getElementById('cardThughness').innerText = card.toughness == undefined ? "NotSet" : card.toughness;
+        document.getElementById('cardManaCost').innerText = card.manaCost == undefined ? "NotSet" : card.manaCost;
+        document.getElementById('cardText').innerText = card.text == undefined ? "NotSet" : card.text;
+
+        // name
+        // cardImage
+        // cardType
+        // cardSubtype
+        // cardColor
+        // cardSupertypes
+        // cardRarity
+        // cardSet
+        // cardPower
+        // cardThughness
+        // cardManaCost
+        // cardText
         
     })
