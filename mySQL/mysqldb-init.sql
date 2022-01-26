168275f02c6ba1deb9e4c9cbd15583d5766d2511
EskaValier <d.soczek1999@gmail.com>
2022-01-26 14:48:42 +0100

    db

--- mySQL/mysqldb-init.sql
+++ mySQL/mysqldb-init.sql
@@ -1,17 +1,13 @@
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Admin123');
-
 CREATE DATABASE mtgsearch;
 USE mtgsearch;
 
 CREATE TABLE `opinions` (
   `OpinionID` INT NOT NULL,
-  `CardID` VARCHAR,
-  `Opinion` VARCHAR,
+  `CardID` TEXT(255),
+  `Opinion` TEXT(255),
   PRIMARY KEY (`OpinionID`)
 );
 
 INSERT INTO opinions VALUES (1, "fcb329b5-43b5-57d6-b7e5-5bb216cf05ff", "good card, I recommend");
 INSERT INTO opinions VALUES (2, "fcb329b5-43b5-57d6-b7e5-5bb216cf05ff", "the best card from this set, beautiful graphics");
 INSERT INTO opinions VALUES (3, "fcb329b5-43b5-57d6-b7e5-5bb216cf05ff", "weak card, how can you play with it");
-
-Select * from opinions;
